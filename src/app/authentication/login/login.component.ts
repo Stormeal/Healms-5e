246bf937b20c51b1ff7a240fb1ee6a0137d25a4b
Alex Storm <alex.ellegaard@yahoo.com>
2019-04-29 22:04:39 +0200

    0.1.0

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, ElementRef, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AuthService } from 'src/app/core/auth.service';
 
 declare var $: any;
 
@@ -13,19 +17,19 @@ export class LoginComponent implements OnInit, OnDestroy {
     private sidebarVisible: boolean;
     private nativeElement: Node;
 
-    constructor(private element: ElementRef) {
+    constructor(private element: ElementRef, private auth: AuthService, private router: Router) {
         this.nativeElement = element.nativeElement;
         this.sidebarVisible = false;
     }
 
     ngOnInit() {
-        var navbar : HTMLElement = this.element.nativeElement;
+        var navbar: HTMLElement = this.element.nativeElement;
         this.toggleButton = navbar.getElementsByClassName('navbar-toggle')[0];
         const body = document.getElementsByTagName('body')[0];
         body.classList.add('login-page');
         body.classList.add('off-canvas-sidebar');
         const card = document.getElementsByClassName('card')[0];
-        setTimeout(function() {
+        setTimeout(function () {
             // after 1000 ms we add the class animated to the login/register card
             card.classList.remove('card-hidden');
         }, 700);
@@ -35,7 +39,7 @@ export class LoginComponent implements OnInit, OnDestroy {
         var body = document.getElementsByTagName('body')[0];
         var sidebar = document.getElementsByClassName('navbar-collapse')[0];
         if (this.sidebarVisible == false) {
-            setTimeout(function() {
+            setTimeout(function () {
                 toggleButton.classList.add('toggled');
             }, 500);
             body.classList.add('nav-open');
@@ -46,9 +50,22 @@ export class LoginComponent implements OnInit, OnDestroy {
             body.classList.remove('nav-open');
         }
     }
-    ngOnDestroy(){
-      const body = document.getElementsByTagName('body')[0];
-      body.classList.remove('login-page');
-      body.classList.remove('off-canvas-sidebar');
+    ngOnDestroy() {
+        const body = document.getElementsByTagName('body')[0];
+        body.classList.remove('login-page');
+        body.classList.remove('off-canvas-sidebar');
+    }
+
+    async signInWithGoogle() {
+        await this.auth.googleLogin();
+        return await this.afterSignIn();
+    }
+
+
+    /// Shared
+
+    private afterSignIn() {
+        // Do after login stuff here, such router redirects, toast messages, etc.
+        return this.router.navigate(['/']);
     }
 }
