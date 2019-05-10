077f5076bd70b740d8c1857b3606f3e086719621
Alex Storm <alex.ellegaard@yahoo.com>
2019-05-10 22:25:09 +0200

    0.1.0

--- src/app/world/spellcasting/spellcasting.component.ts
+++ src/app/world/spellcasting/spellcasting.component.ts
@@ -1,23 +1,106 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, ViewEncapsulation } from '@angular/core';
 import * as data from '../../../assets/srd/spellcasting.json';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA, MatDialogConfig } from '@angular/material';
+import { Observable } from 'rxjs';
 
 declare var require: any;
 
 @Component({
   selector: 'app-spellcasting',
   templateUrl: './spellcasting.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./spellcasting.component.scss']
 })
 export class SpellcastingComponent implements OnInit {
-  data = require('../../../assets/srd/spellcasting.json');
+  spellCasting = require('../../../assets/srd/spellcasting.json');
   spellList = require('../../../assets/srd/spellList.json');
+  spells = require('../../../assets/srd/spells.json');
+  spells2 = require('../../../assets/srd/spells2.json');
 
-  constructor() { }
+  bardCantrips: Observable<any>;
+  bardCantripsDescription;
+
+
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
-    console.log(this.spellList.Bard.Cantrips);
+    this.loader();
+    this.getBardSpells();
+
 
   }
 
+  loader() {
+    const required = this.spellList.Bard.Cantrips[0];
+
+    console.log('Required: ', this.spellList.Bard.Cantrips[0]);
+
+    console.log('Spell', this.spells2.spell);
+
+    const result = this.spells2.spell.find(spell => spell.name[0] === required);
+
+    console.log('Result', result);
+
+
+  }
+
+  mouseEnter(spell: any) {
+    console.log('Spello', spell);
+
+    const required = spell;
+
+    const result = this.spells2.spell.find(_spell => _spell.name[0] === required);
+    console.log('Spell Description', result);
+
+    return this.bardCantripsDescription = result;
+
+
+  }
+
+  getBardSpells() {
+    const cantrips = this.spellList.Bard.Cantrips;
+    const spellDescription = this.spells2.spell;
+
+    // this.bardCantripsDescription = spellDescription.find(spell => spell.name[0] === this.bardCantrips);
+
+    this.bardCantrips = this.spellList.Bard.Cantrips;
+
+
+
+
+
+
+    // this.bardCantripsDescription = this.spells2.spell.find(spell => spell.name[0] === cantrips);
+
+    console.log('Test 1', this.bardCantripsDescription);
+
+
+
+
+
+  }
+
+  openDialog(spell: any, dialogConfig: MatDialogConfig) {
+    const dialogRef = this.dialog.open(SpellDialogComponent, {
+      data: spell
+    });
+    dialogRef.componentInstance.dialogConfig = dialogConfig;
+  }
+
+}
+
+@Component({
+  selector: 'app-spell',
+  templateUrl: 'spell-dialog.html'
+})
+export class SpellDialogComponent {
+  dialogConfig: MatDialogConfig;
+
+  constructor(
+    public dialogRef: MatDialogRef<SpellDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public spell: any) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
