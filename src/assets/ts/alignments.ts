528404380a5beaaf9fc78304a1de3cc99e6158de
stormeal <alex.elllegaard@yahoo.com>
2019-06-25 10:49:17 +0200

    0.2.0

---
+++ src/assets/ts/alignments.ts
@@ -0,0 +1,11 @@
+export const Alignments = [
+  { value: "Lawful Good", viewValue: "Lawful Good" },
+  { value: "Lawful Neutral", viewValue: "Lawful Neutral" },
+  { value: "Lawful Evil", viewValue: "Lawful Evil" },
+  { value: "Neutral Good", viewValue: "Neutral Good" },
+  { value: "(True) Neutral", viewValue: "(True) Neutral" },
+  { value: "Neutral Evil", viewValue: "Neutral Evil" },
+  { value: "Chaotic Good", viewValue: "Chaotic Good" },
+  { value: "Chaotic Neutral", viewValue: "Chaotic Neutral" },
+  { value: "Chaotic Evil", viewValue: "Chaotic Evil" }
+];
