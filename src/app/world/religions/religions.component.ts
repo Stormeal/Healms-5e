0bba187cb5f4e0b7efe4644510313911f549eaae
stormeal <alex.elllegaard@yahoo.com>
2019-08-07 15:41:15 +0200

    0.2.0

--- src/app/world/religions/religions.component.ts
+++ src/app/world/religions/religions.component.ts
@@ -1,14 +1,98 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormArray, FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { AuthService } from "src/app/core/auth.service";
+import { AngularFirestore } from "@angular/fire/firestore";
+
+import * as faker from "faker";
 
 @Component({
-  selector: 'app-religions',
-  templateUrl: './religions.component.html',
+  selector: "app-religions",
+  templateUrl: "./religions.component.html",
 })
 export class ReligionsComponent implements OnInit {
+  religionForm: FormGroup;
+  deitiesList: FormArray;
+  user: any;
+  campaignId: any;
+  campaign: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  get deityFormGroup() {
+    return this.religionForm.get("deities") as FormArray;
   }
 
+  constructor(
+    private auth: AuthService,
+    private fb: FormBuilder,
+    private afs: AngularFirestore,
+  ) {}
+
+  ngOnInit() {
+    this.religionForm = this.fb.group({
+      religionName: "",
+      mythology: "",
+      origins: "",
+      ethics: "",
+      divinePowers: "",
+      influence: "",
+      cosmology: "",
+      tenetsOfFaith: "",
+      deities: this.fb.array([this.createDeity()]),
+    });
+
+    this.deitiesList = this.religionForm.get("deities") as FormArray;
+  }
+
+  createDeity(): FormGroup {
+    return this.fb.group({
+      deityName: ["", Validators.compose([Validators.required])],
+      deityAlignment: ["", Validators.compose([Validators.required])],
+      deityDomain: ["", Validators.compose([Validators.required])],
+      deitySymbol: ["", Validators.compose([Validators.required])],
+    });
+  }
+
+  addDeity() {
+    this.deitiesList.push(this.createDeity());
+  }
+
+  removeDeity(index) {
+    this.deitiesList.removeAt(index);
+  }
+
+  newReligion() {
+    this.auth.getUser().subscribe(user => {
+      this.user = user;
+      this.campaignId = this.user.campaigns.campaignId;
+      this.afs
+        .doc(`campaigns/${this.campaignId}`)
+        .valueChanges()
+        .subscribe(campaign => {
+          this.campaign = campaign;
+          const campId = this.campaign.uid;
+          const religionId = {
+            uid:
+              this.religionForm.value["religionName"] +
+              "_" +
+              faker.random.alphaNumeric(4),
+          };
+          const religion = {
+            uid: religionId,
+            religionName: this.religionForm.value["religionName"],
+            mythology: this.religionForm.value["mythology"],
+            origins: this.religionForm.value["origins"],
+            ethics: this.religionForm.value["ethics"],
+            divinePowers: this.religionForm.value["divinePowers"],
+            influence: this.religionForm.value["influence"],
+            cosmology: this.religionForm.value["cosmology"],
+            tenetsOfFaith: this.religionForm.value["tenetsOfFaith"],
+            deities: this.religionForm.value["deities"],
+          };
+          this.afs
+            .collection(`campaigns/${campId}/religions`)
+            .doc(religionId.uid)
+            .set(religion);
+          return this.religionForm.reset();
+        });
+    });
+  }
 }
